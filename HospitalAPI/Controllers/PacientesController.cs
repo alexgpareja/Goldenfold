2c9bdb30b0ce9615b717e179eb1ea22579cdf6f2
Maria <mini.mar@hotmail.com>
2024-09-03 12:15:26 +0200

    authentication with token tested

--- HospitalAPI/Controllers/PacientesController.cs
+++ HospitalAPI/Controllers/PacientesController.cs
@@ -6,9 +6,11 @@ using AutoMapper;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HospitalApi.Controllers
 {
+    [Authorize(Roles = "Administrativo")]
     [ApiController]
     [Route("api/[controller]")]
     public class PacientesController : ControllerBase
