a4cae4f7e12c2aa5d3414d92087fb435ed65378c
Maria <mini.mar@hotmail.com>
2024-09-04 13:03:24 +0200

    adding mapping for the new dtos

--- HospitalAPI/Mapping/AutoMapperProfile.cs
+++ HospitalAPI/Mapping/AutoMapperProfile.cs
@@ -17,16 +17,23 @@ namespace HospitalApi.Mapping
 
             // Cama mappings
             CreateMap<Cama, CamaDTO>().ReverseMap();
+            CreateMap<Cama, CamaCreateDTO>().ReverseMap();
+            CreateMap<Cama, CamaUpdateDTO>().ReverseMap();
 
             // Habitacion mappings
             CreateMap<Habitacion, HabitacionDTO>().ReverseMap();
+            CreateMap<Habitacion, HabitacionCreateDTO>().ReverseMap();
+            CreateMap<Habitacion, HabitacionUpdateDTO>().ReverseMap();
 
             // Asignacion mappings
             CreateMap<Asignacion, AsignacionDTO>().ReverseMap();
+            CreateMap<Asignacion, AsignacionCreateDTO>().ReverseMap();
+            CreateMap<Asignacion, AsignacionUpdateDTO>().ReverseMap();
 
             // HistorialAlta mappings
             CreateMap<HistorialAlta, HistorialAltaDTO>().ReverseMap();
-
+            CreateMap<HistorialAlta, HistorialAltaCreateDTO>().ReverseMap();
+            CreateMap<HistorialAlta, HistorialAltaUpdateDTO>().ReverseMap();
             // Usuario mappings
             CreateMap<Usuario, UsuarioDTO>().ReverseMap();
             CreateMap<Usuario, UsuarioCreateDTO>().ReverseMap();
