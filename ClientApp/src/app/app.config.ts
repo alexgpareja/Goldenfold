045dbde5fb086bc15462755436149b0a5745800b
Maria <mini.mar@hotmail.com>
2024-09-18 10:46:47 +0200

    keycloak implementation commented wip

--- ClientApp/src/app/app.config.ts
+++ ClientApp/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { KeycloakService } from 'keycloak-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(), KeycloakService]
 };
