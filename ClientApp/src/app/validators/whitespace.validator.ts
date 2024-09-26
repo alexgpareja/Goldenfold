00fc3ad09d22aceda3abd90b4dcceb1eb5ed35b4
Yagogrr <ygog5002@gmail.com>
2024-09-26 09:38:31 +0200

    usuarios validadores[!blank,min 2 words, username exists]

--- ClientApp/src/app/validators/whitespace.validator.ts
+++ ClientApp/src/app/validators/whitespace.validator.ts
@@ -1,6 +1,9 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn, AsyncValidatorFn, FormControl } from '@angular/forms';
+import { catchError, debounceTime, delay, map, Observable, of, switchMap } from 'rxjs';
+import { ApiService } from '../services/api.service';
 
 export class CustomValidators {
+
   // Validador para no permitir solo espacios en blanco
   static noWhitespaceValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
@@ -9,4 +12,31 @@ export class CustomValidators {
       return isValid ? null : { whitespace: true };
     };
   }
+
+  // Validador asíncrono
+  static asyncFieldExisting(apiService: ApiService) {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+        return of(control.value).pipe(
+            debounceTime(300),
+            switchMap(value => 
+                apiService.getUsuarios(undefined, value, undefined).pipe(
+                    map(usuarios => {
+                        // Filtrar los usuarios en el frontend
+                        const usuariosFiltrados = usuarios.filter(usuario => 
+                            usuario.NombreUsuario==value
+                        );
+
+                        if (usuariosFiltrados.length > 0) {
+                            return { asyncFieldExisting: true };
+                        }
+                        return null;
+                    }),
+                    catchError(() => of(null))
+                )
+            )
+        );
+    };
+}
+
+
 }
