bc57672b7280a6acd4f778b91d7944970097d947
Alex Gil <alexgpareja@icloud.com>
2024-09-26 10:59:25 +0200

    roles validations funcionan bien

--- ClientApp/src/app/validators/rolname-exists.validator.ts
+++ ClientApp/src/app/validators/rolname-exists.validator.ts
@@ -12,18 +12,22 @@ export const asyncRolNameExistsValidator = (
   apiService: ApiService
 ): AsyncValidatorFn => {
   return (control: AbstractControl): Observable<ValidationErrors | null> => {
-    const nombreRol = control.value?.trim();
-
-    if (!nombreRol) {
-      return of(null); // No hacer validación si el campo está vacío
-    }
-
-    return of(nombreRol).pipe(
-      debounceTime(300), // Evitar múltiples llamadas rápidas
+    return of(control.value).pipe(
+      debounceTime(300),
       switchMap((value) =>
         apiService.getRoles(value).pipe(
-          map((roles) => (roles.length > 0 ? { rolExists: true } : null)),
-          catchError(() => of(null)) // Si la API falla, no detener la validación
+          map((roles) => {
+            // Filtrar los roles en el frontend
+            const rolesFiltrados = roles.filter(
+              (rol) => rol.NombreRol == value
+            );
+
+            if (rolesFiltrados.length > 0) {
+              return { asyncFieldExisting: true };
+            }
+            return null;
+          }),
+          catchError(() => of(null))
         )
       )
     );
