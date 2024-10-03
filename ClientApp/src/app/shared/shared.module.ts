5d57ef71224fe86bf000c4fd8f2e45c56b199f4c
Yagogrr <ygog5002@gmail.com>
2024-10-03 09:15:55 +0200

    fixing modules

--- ClientApp/src/app/shared/shared.module.ts
+++ ClientApp/src/app/shared/shared.module.ts
@@ -9,12 +9,10 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { SearchBoxComponent } from './search-box/search-box.component';
 
 // Angular Material Imports
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
+
 import { MatDialogModule, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { DialogFormularioComponent } from './dialog-formulario/dialog-formulario.component';
 
 @NgModule({
